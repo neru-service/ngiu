47e8c36bd5a44771f65eedb2f29252beab9e0eb4
Neru Kun <95429724+neru-service@users.noreply.github.com>
2023-12-30 05:18:02 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 // pages/_app.js
 
-import '../styles/globals.css'; // グローバルなスタイルを適用するためのファイル
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import '../styles/globals.css'; // あなたのグローバルなスタイルファイル
+
+// Create a new QueryClient instance
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools /> {/* 開発環境でのみ表示 */}
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
